e9d6c9df1d2654e9af7f5ff48ca502fec33c8324
Bryce Ferenczi <bryce.ferenczi@monashmotorsport.com>
2020-08-16 01:21:57 +1000

    Fixing static method issue, now complains about wanting 8 grads rather than 2, I think I need to set some as requires_grad_(False) or something..

--- nnet_training_framework/correlation_package/correlation.py
+++ nnet_training_framework/correlation_package/correlation.py
@@ -4,19 +4,13 @@ from torch.autograd import Function
 import correlation_cuda
 
 class CorrelationFunction(Function):
+    """
+    Typical Parameters: pad_size=3, kernel_size=3, max_displacement=20, stride1=1, stride2=2, corr_multiply=1
+    """
 
-    def __init__(self, pad_size=3, kernel_size=3, max_displacement=20, stride1=1, stride2=2, corr_multiply=1):
-        super(CorrelationFunction, self).__init__()
-        self.pad_size = pad_size
-        self.kernel_size = kernel_size
-        self.max_displacement = max_displacement
-        self.stride1 = stride1
-        self.stride2 = stride2
-        self.corr_multiply = corr_multiply
-        # self.out_channel = ((max_displacement/stride2)*2 + 1) * ((max_displacement/stride2)*2 + 1)
-
-    def forward(ctx, input1, input2):
-        self.save_for_backward(input1, input2)
+    @staticmethod
+    def forward(ctx, input1, input2, pad_size=3, kernel_size=3, max_displacement=20, stride1=1, stride2=2, corr_multiply=1):
+        ctx.save_for_backward(input1, input2)
 
         with torch.cuda.device_of(input1):
             rbot1 = input1.new()
@@ -24,12 +18,13 @@ class CorrelationFunction(Function):
             output = input1.new()
 
             correlation_cuda.forward(input1, input2, rbot1, rbot2, output, 
-                self.pad_size, self.kernel_size, self.max_displacement,self.stride1, self.stride2, self.corr_multiply)
+                pad_size, kernel_size, max_displacement, stride1, stride2, corr_multiply)
 
         return output
 
-    def backward(self, grad_output):
-        input1, input2 = self.saved_tensors
+    @staticmethod
+    def backward(ctx, grad_output, pad_size=3, kernel_size=3, max_displacement=20, stride1=1, stride2=2, corr_multiply=1):
+        input1, input2 = ctx.saved_tensors
 
         with torch.cuda.device_of(input1):
             rbot1 = input1.new()
@@ -39,7 +34,7 @@ class CorrelationFunction(Function):
             grad_input2 = input2.new()
 
             correlation_cuda.backward(input1, input2, rbot1, rbot2, grad_output, grad_input1, grad_input2,
-                self.pad_size, self.kernel_size, self.max_displacement,self.stride1, self.stride2, self.corr_multiply)
+                pad_size, kernel_size, max_displacement, stride1, stride2, corr_multiply)
 
         return grad_input1, grad_input2
 
@@ -47,6 +42,14 @@ class CorrelationFunction(Function):
 class Correlation(Module):
     def __init__(self, pad_size=0, kernel_size=0, max_displacement=0, stride1=1, stride2=2, corr_multiply=1):
         super(Correlation, self).__init__()
+        # self.args = {
+        #     'pad_size': pad_size,
+        #     'kernel_size': kernel_size,
+        #     'max_displacement': max_displacement,
+        #     'stride1': stride1,
+        #     'stride2': stride2,
+        #     'corr_multiply': corr_multiply
+        # }
         self.pad_size = pad_size
         self.kernel_size = kernel_size
         self.max_displacement = max_displacement
@@ -54,10 +57,11 @@ class Correlation(Module):
         self.stride2 = stride2
         self.corr_multiply = corr_multiply
 
-    @staticmethod
+        self.correlation = CorrelationFunction.apply
+
     def forward(self, input1, input2):
-        result = CorrelationFunction(self.pad_size, self.kernel_size, self.max_displacement, self.stride1, self.stride2, self.corr_multiply)(input1, input2)
-        return result
+        # return self.correlation(input1, input2, self.args)
+        return self.correlation(input1, input2, self.pad_size, self.kernel_size, self.max_displacement, self.stride1, self.stride2, self.corr_multiply)
 
 if __name__ == '__main__':
     import time
