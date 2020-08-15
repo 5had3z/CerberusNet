1d87d9cb738b9f104e8b0d464d370627c42a84f3
Bryce Ferenczi <bryce.ferenczi@monashmotorsport.com>
2020-08-16 01:35:08 +1000

    Now to sove an illegal copy...?

--- nnet_training_framework/correlation_package/correlation.py
+++ nnet_training_framework/correlation_package/correlation.py
@@ -11,6 +11,12 @@ class CorrelationFunction(Function):
     @staticmethod
     def forward(ctx, input1, input2, pad_size=3, kernel_size=3, max_displacement=20, stride1=1, stride2=2, corr_multiply=1):
         ctx.save_for_backward(input1, input2)
+        ctx.pad_size = pad_size
+        ctx.kernel_size = kernel_size
+        ctx.max_displacement = max_displacement
+        ctx.stride1 = stride1
+        ctx.stride2 = stride2
+        ctx.corr_multiply = corr_multiply
 
         with torch.cuda.device_of(input1):
             rbot1 = input1.new()
@@ -23,7 +29,7 @@ class CorrelationFunction(Function):
         return output
 
     @staticmethod
-    def backward(ctx, grad_output, pad_size=3, kernel_size=3, max_displacement=20, stride1=1, stride2=2, corr_multiply=1):
+    def backward(ctx, grad_output):
         input1, input2 = ctx.saved_tensors
 
         with torch.cuda.device_of(input1):
@@ -34,9 +40,9 @@ class CorrelationFunction(Function):
             grad_input2 = input2.new()
 
             correlation_cuda.backward(input1, input2, rbot1, rbot2, grad_output, grad_input1, grad_input2,
-                pad_size, kernel_size, max_displacement, stride1, stride2, corr_multiply)
+                ctx.pad_size, ctx.kernel_size, ctx.max_displacement, ctx.stride1, ctx.stride2, ctx.corr_multiply)
 
-        return grad_input1, grad_input2
+        return grad_input1, grad_input2, None, None, None, None, None, None 
 
 
 class Correlation(Module):
