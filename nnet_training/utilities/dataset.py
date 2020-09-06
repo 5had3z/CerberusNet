ee6bb90b6f6db2989839716d6e06c1648a5ff9d1
Bryce Ferenczi <bryce.ferenczi@monashmotorsport.com>
2020-09-06 12:26:47 +1000

    Refactored dataset and added random rotation

--- nnet_training/utilities/dataset.py
+++ nnet_training/utilities/dataset.py
@@ -30,8 +30,7 @@ class CityScapesDataset(torch.utils.data.Dataset):
     Get Item will return the corresponding dictionary with keys:
         [l_img, r_img, l_seq", r_seq, cam, pose, disparity]
     """
-    def __init__(self, directories: Path, output_size=(1024, 512), crop_fraction=2, id_vector=None,\
-            transform=torchvision.transforms.ToTensor(), disparity_out=False):
+    def __init__(self, directories: Path, output_size=(1024, 512), disparity_out=False, **kwargs):
         '''
         Initializer for Cityscapes dataset\n
         @input dictionary that contain paths of datasets as
@@ -42,43 +41,29 @@ class CityScapesDataset(torch.utils.data.Dataset):
             e.g. 2 results in width/2 by height/2 random crop of original image\n
         '''
         l_img_key = None
-        self.enable_right       = False
-        self.enable_seg         = False
-        self.enable_disparity   = False
-        self.enable_left_seq    = False
-        self.enable_right_seq   = False
-        self.enable_cam         = False
-        self.enable_pose        = False
 
         for key in directories.keys():
-            if key == 'left_images' or key == 'images':
-                l_img = []
+            if key in ['left_images', 'images']:
                 l_img_key = key
+                self.l_img = []
             elif key == 'seg':
-                seg = []
                 seg_dir_key = key
-                self.enable_seg = True
+                self.seg = []
             elif key == 'right_images':
-                r_img = []
-                self.enable_right = True
+                self.r_img = []
             elif key == 'disparity':
-                disp = []
-                self.enable_disparity = True
+                self.disp = []
             elif key == 'left_seq':
-                l_seq = []
-                self.enable_left_seq = True
+                self.l_seq = []
             elif key == 'right_seq':
-                r_seq = []
-                self.enable_right_seq = True
+                self.r_seq = []
             elif key == 'cam':
-                cam = []
-                self.enable_cam = True
+                self.cam = []
             elif key == 'pose':
-                pose = []
-                self.enable_pose = True
+                self.pose = []
 
-        if not self.enable_seg and not self.enable_disparity and \
-                not (self.enable_left_seq or self.enable_right_seq):
+        if not hasattr(self, 'seg') and not hasattr(self, 'disp') and \
+                not (hasattr(self, 'l_seq') or hasattr(self, 'r_seq')):
             print("Neither Segmentation, Disparity or Img Sequence Keys are defined")
 
         if l_img_key is None:
@@ -92,49 +77,49 @@ class CityScapesDataset(torch.utils.data.Dataset):
                     l_imgpath = os.path.join(dir_name, filename)
                     foldername = os.path.basename(os.path.dirname(l_imgpath))
 
-                    if self.enable_right:
+                    if hasattr(self, 'r_img'):
                         r_imgname = filename.replace('leftImg8bit', 'rightImg8bit')
                         r_imgpath = os.path.join(directories['right_images'], foldername, r_imgname)
                         if not os.path.isfile(r_imgpath):
                             read_check = False
                             print("Error finding corresponding right image to ", l_imgpath)
 
-                    if self.enable_seg:
+                    if hasattr(self, 'seg'):
                         seg_name = filename.replace('leftImg8bit', 'gtFine_labelIds')
                         seg_path = os.path.join(directories[seg_dir_key], foldername, seg_name)
                         if not os.path.isfile(seg_path):
                             read_check = False
                             print("Error finding corresponding segmentation image to ", l_imgpath)
 
-                    if self.enable_disparity:
+                    if hasattr(self, 'disp'):
                         disp_name = filename.replace('leftImg8bit', 'disparity')
                         disp_path = os.path.join(directories['disparity'], foldername, disp_name)
                         if not os.path.isfile(disp_path):
                             read_check = False
                             print("Error finding corresponding disparity image to ", l_imgpath)
 
-                    if self.enable_left_seq:
+                    if hasattr(self, 'l_seq'):
                         left_seq_name = filename.replace('19_leftImg8bit', '20_leftImg8bit')
                         left_seq_path = os.path.join(directories['left_seq'], foldername, left_seq_name)
                         if not os.path.isfile(left_seq_path):
                             read_check = False
                             print("Error finding corresponding left sequence image to ", l_imgpath)
 
-                    if self.enable_right_seq:
+                    if hasattr(self, 'r_seq'):
                         right_seq_name = filename.replace('19_leftImg8bit', '20_rightImg8bit')
                         right_seq_path = os.path.join(directories['right_seq'], foldername, right_seq_name)
                         if not os.path.isfile(right_seq_path):
                             read_check = False
                             print("Error finding corresponding right sequence image to ", l_imgpath)
 
-                    if self.enable_cam:
+                    if hasattr(self, 'cam'):
                         cam_name = filename.replace('leftImg8bit.png', 'camera.json')
                         cam_path = os.path.join(directories['cam'], foldername, cam_name)
                         if not os.path.isfile(cam_path):
                             read_check = False
                             print("Error finding corresponding camera parameters for ", l_imgpath)
 
-                    if self.enable_pose:
+                    if hasattr(self, 'pose'):
                         pose_name = filename.replace('leftImg8bit.png', 'vehicle.json')
                         pose_path = os.path.join(directories['pose'], foldername, pose_name)
                         if not os.path.isfile(pose_path):
@@ -142,56 +127,49 @@ class CityScapesDataset(torch.utils.data.Dataset):
                             print("Error finding corresponding GPS/Pose information for ", l_imgpath)
 
                     if read_check:
-                        l_img.append(l_imgpath)
-                        if self.enable_right:
-                            r_img.append(r_imgpath)
-                        if self.enable_seg:
-                            seg.append(seg_path)
-                        if self.enable_disparity:
-                            disp.append(disp_path)
-                        if self.enable_left_seq:
-                            l_seq.append(left_seq_path)
-                        if self.enable_right_seq:
-                            r_seq.append(right_seq_path)
-                        if self.enable_cam:
-                            cam.append(cam_path)
-                        if self.enable_pose:
-                            pose.append(pose_path)
-        
-        #Create dataset from specified ids
-        if id_vector is not None:
-            self.l_img = [l_img[i] for i in id_vector]
-            if self.enable_right:
-                self.r_img = [r_img[i] for i in id_vector]
-            if self.enable_seg:
-                self.seg = [seg[i] for i in id_vector]
-            if self.enable_disparity:
-                self.disp = [disp[i] for i in id_vector]
-            if self.enable_left_seq:
-                self.l_seq = [l_seq[i] for i in id_vector]
-            if self.enable_right_seq:
-                self.r_seq = [r_seq[i] for i in id_vector]
-        else:
-            self.l_img = l_img
-            if self.enable_right:
-                self.r_img = r_img
-            if self.enable_seg:
-                self.seg = seg
-            if self.enable_disparity:
-                self.disp = disp
-            if self.enable_left_seq:
-                self.l_seq = l_seq
-            if self.enable_right_seq:
-                self.r_seq = r_seq
-            if self.enable_cam:
-                self.cam = cam
-            if self.enable_pose:
-                self.pose = pose
+                        self.l_img.append(l_imgpath)
+                        if hasattr(self, 'r_img'):
+                            self.r_img.append(r_imgpath)
+                        if hasattr(self, 'seg'):
+                            self.seg.append(seg_path)
+                        if hasattr(self, 'disp'):
+                            self.disp.append(disp_path)
+                        if hasattr(self, 'l_seq'):
+                            self.l_seq.append(left_seq_path)
+                        if hasattr(self, 'r_seq'):
+                            self.r_seq.append(right_seq_path)
+                        if hasattr(self, 'cam'):
+                            self.cam.append(cam_path)
+                        if hasattr(self, 'pose'):
+                            self.pose.append(pose_path)
 
-        self.transform = transform
+        # Create dataset from specified ids if id_vector given else use all
+        if 'id_vector' in kwargs:
+            self.l_img = [self.l_img[i] for i in kwargs['id_vector']]
+            if hasattr(self, 'r_img'):
+                self.r_img = [self.r_img[i] for i in kwargs['id_vector']]
+            if hasattr(self, 'seg'):
+                self.seg = [self.seg[i] for i in kwargs['id_vector']]
+            if hasattr(self, 'disp'):
+                self.disp = [self.disp[i] for i in kwargs['id_vector']]
+            if hasattr(self, 'l_seq'):
+                self.l_seq = [self.l_seq[i] for i in kwargs['id_vector']]
+            if hasattr(self, 'r_seq'):
+                self.r_seq = [self.r_seq[i] for i in kwargs['id_vector']]
+            if hasattr(self, 'cam'):
+                self.cam = [self.cam[i] for i in kwargs['id_vector']]
+            if hasattr(self, 'pose'):
+                self.pose = [self.pose[i] for i in kwargs['id_vector']]
+
         self.disparity_out = disparity_out
         self.output_size = output_size
-        self.crop_fraction = crop_fraction
+
+        if 'crop_fraction' in kwargs:
+            self.crop_fraction = kwargs['crop_fraction']
+        if 'rand_rotation' in kwargs:
+            self.rand_rot = kwargs['rand_rotation']
+        if 'rand_brightness' in kwargs:
+            self.brightness = kwargs['rand_brightness']
 
         # valid_classes = [7, 8, 11, 12, 13, 17, 19, 20, 21, 22,
         #                       23, 24, 25, 26, 27, 28, 31, 32, 33]
@@ -215,28 +193,28 @@ class CityScapesDataset(torch.utils.data.Dataset):
         # Read image and labels
         epoch_data["l_img"] = Image.open(self.l_img[idx]).convert('RGB')
 
-        if self.enable_right:
+        if hasattr(self, 'r_img'):
             epoch_data["r_img"] = Image.open(self.r_img[idx]).convert('RGB')
-        if self.enable_seg:
+        if hasattr(self, 'seg'):
             epoch_data["seg"] = Image.open(self.seg[idx])
-        if self.enable_disparity:
+        if hasattr(self, 'disp'):
             epoch_data["disparity"] = Image.open(self.disp[idx])
-        if self.enable_left_seq:
+        if hasattr(self, 'l_seq'):
             epoch_data["l_seq"] = Image.open(self.l_seq[idx]).convert('RGB')
-        if self.enable_right_seq:
+        if hasattr(self, 'r_seq'):
             epoch_data["r_seq"] = Image.open(self.r_seq[idx]).convert('RGB')
 
         self._sync_transform(epoch_data)
 
-        #   Apply Defined Transformations
-        if self.transform is not None:
-            for key in epoch_data.keys():
-                if key in ["l_img", "r_img", "l_seq", "r_seq"]:
-                    epoch_data[key] = self.transform(epoch_data[key])
+        #   Transform images to tensors
+        for key in epoch_data.keys():
+            if key in ["l_img", "r_img", "l_seq", "r_seq"]:
+                epoch_data[key] = torchvision.transforms.functional.to_tensor(epoch_data[key])
 
-        if self.enable_cam:
+        if hasattr(self, 'cam'):
             epoch_data["cam"] = self.json_to_intrinsics(self.cam[idx])
-        # if self.enable_pose:
+
+        # if hasattr(self, 'pose'):
         #     epoch_data["pose"] = self.json_to_pose(self.pose[idx])
 
         return epoch_data
@@ -247,33 +225,48 @@ class CityScapesDataset(torch.utils.data.Dataset):
             for item in epoch_data.values():
                 item = item.transpose(Image.FLIP_LEFT_RIGHT)
 
-        # random crop
-        crop_h = int(epoch_data["l_img"].size[0]/self.crop_fraction)
-        crop_w = int(epoch_data["l_img"].size[1]/self.crop_fraction)
-        crop_x = random.randint(0, epoch_data["l_img"].size[1] - crop_w)
-        crop_y = random.randint(0, epoch_data["l_img"].size[0] - crop_h)
+        if hasattr(self, 'crop_fraction'):
+            # random crop
+            crop_h = int(epoch_data["l_img"].size[0]/self.crop_fraction)
+            crop_w = int(epoch_data["l_img"].size[1]/self.crop_fraction)
+            crop_x = random.randint(0, epoch_data["l_img"].size[1] - crop_w)
+            crop_y = random.randint(0, epoch_data["l_img"].size[0] - crop_h)
 
-        for item in epoch_data.values():
-            item = item.crop((crop_y, crop_x, crop_y+crop_h, crop_x+crop_w))
+            for item in epoch_data.values():
+                item = item.crop((crop_y, crop_x, crop_y+crop_h, crop_x+crop_w))
 
-        brightness_scale = random.uniform(0.8, 1.2)
+        if hasattr(self, 'rand_rot'):
+            angle = random.uniform(0, self.rand_rot)
+            for key in epoch_data.keys():
+                if key in ["l_img", "r_img", "l_seq", "r_seq"]:
+                    item = torchvision.transforms.functional.rotate(
+                        epoch_data['key'], angle, resample=Image.BILINEAR)
+                else:
+                    item = torchvision.transforms.functional.rotate(
+                        epoch_data['key'], angle, resample=Image.NEAREST, fill=-1)
+
+        if hasattr(self, 'brightness'):
+            brightness_scale = random.uniform(1-self.brightness/100, 1+self.brightness/100)
+            for key in epoch_data.keys():
+                if key in ["l_img", "r_img", "l_seq", "r_seq"]:
+                    epoch_data[key] = torchvision.transforms.functional.adjust_brightness(
+                        epoch_data[key], brightness_scale)
 
         for key in epoch_data.keys():
-            if key in ["l_img", "r_img", "l_seq", "r_seq"]:
-                epoch_data[key] = torchvision.transforms.functional.adjust_brightness(epoch_data[key], brightness_scale)
+            if key in ["l_img", "r_img", "l_seq", "r_seq"]:                  
                 epoch_data[key] = epoch_data[key].resize(self.output_size, Image.BILINEAR)
                 epoch_data[key] = self._img_transform(epoch_data[key])
-            elif key is "seg":
+            elif key == "seg":
                 epoch_data[key] = epoch_data[key].resize(self.output_size, Image.NEAREST)
                 epoch_data[key] = self._seg_transform(epoch_data[key])
-            elif key is "disparity":
+            elif key == "disparity":
                 epoch_data[key] = epoch_data[key].resize(self.output_size, Image.NEAREST)
                 epoch_data[key] = self._depth_transform(epoch_data[key])
 
     def _class_to_index(self, seg):
         values = np.unique(seg)
         for value in values:
-            assert (value in self._mapping)
+            assert value in self._mapping
         index = np.digitize(seg.ravel(), self._mapping, right=True)
         return self._key[index].reshape(seg.shape)
 
@@ -296,7 +289,7 @@ class CityScapesDataset(torch.utils.data.Dataset):
             disparity[:,:side_clip]         = -1    #lhs
             disparity[:,-side_clip:-1]      = -1    #rhs
         return  torch.FloatTensor(disparity.astype('float32'))
-    
+
     def json_to_intrinsics(self, json_path):
         with open(json_path) as json_file:
             #   Camera Intrinsic Matrix
@@ -318,7 +311,8 @@ class CityScapesDataset(torch.utils.data.Dataset):
 
 def id_vec_generator(train_ratio, val_ratio, test_ratio, directory):
     num_images = 0
-    for file in os.listdir(directory): num_images += file.endswith(IMG_EXT)
+    for file in os.listdir(directory):
+        num_images += file.endswith(IMG_EXT)
 
     print("Number of Images:\t", num_images)
     image_ids = list(range(num_images))
