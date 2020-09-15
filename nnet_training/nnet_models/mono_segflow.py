Bugfix flipping of images also requires flipping of x direction of flow. Added back in occlusion to the photometric losses, however just give a matrix of ones since the occlusion calculation doesn't work (this is just more explcit so I can revisit later). Removed seg arg for mono segflow, more explicit in not using it, will possibly use in a future arch.

nnet_training/nnet_models/mono_segflow.py
@@ -129,7 +129,7 @@ class MonoSFNet(nn.Module):
         '''
         return self.scale_levels
 
-    def aux_forward(self, im1_pyr, im2_pyr, seg_gt):
+    def aux_forward(self, im1_pyr, im2_pyr):
         '''
         Auxillary forward method that does the flow prediction
         @todo incorperate segmentation
@@ -187,8 +187,8 @@ class MonoSFNet(nn.Module):
         if seg_gt is None:
             seg_gt = self.segmentation_network(im1_pyr)
 
-        flows['flow_fw'] = self.aux_forward(im1_pyr, im2_pyr, seg_gt)
+        flows['flow_fw'] = self.aux_forward(im1_pyr, im2_pyr)
         if consistency:
-            flows['flow_bw'] = self.aux_forward(im2_pyr, im1_pyr, seg_gt)
+            flows['flow_bw'] = self.aux_forward(im2_pyr, im1_pyr)
 
         return flows, seg_gt

nnet_training/utilities/KITTI.py
@@ -138,6 +138,7 @@ class Kitti2015Dataset(torch.utils.data.Dataset):
         self.output_size = tuple(output_size)
 
         self.std_kitti_dims = (1274, 375)
+        self.mirror_x = 1.0
 
         if 'crop_fraction' in kwargs:
             self.crop_fraction = kwargs['crop_fraction']
@@ -189,8 +190,11 @@ class Kitti2015Dataset(torch.utils.data.Dataset):
     def _sync_transform(self, epoch_data):
         # random mirror
         if random.random() < 0.5:
+            self.mirror_x = -1.0
             for key, data in epoch_data.items():
                 epoch_data[key] = data.transpose(Image.FLIP_LEFT_RIGHT)
+        else:
+            self.mirror_x = 1.0
 
         if hasattr(self, 'crop_fraction'):
             # random crop
@@ -252,7 +256,7 @@ class Kitti2015Dataset(torch.utils.data.Dataset):
         scale_y = self.output_size[1] / self.std_kitti_dims[1]
 
         # Apply transform indicated by the devkit including ignore mask
-        flow_out_x = scale_x * (np.array(flow_x).astype('float32') - 2**15) / 64.0
+        flow_out_x = self.mirror_x * scale_x * (np.array(flow_x).astype('float32') - 2**15) / 64.0
         flow_out_y = scale_y * (np.array(flow_y).astype('float32') - 2**15) / 64.0
 
         for key in ["flow_x", "flow_y", "flow_b"]:

nnet_training/utilities/UnFlowLoss.py
@@ -217,20 +217,23 @@ class unFlowLoss(nn.modules.Module):
         self.consistency = consistency
         self.back_occ_only = back_occ_only
 
-    def loss_photometric(self, im_orig, im_recons, occu_mask):
+    def loss_photometric(self, im_orig: torch.Tensor,
+                         im_recons: torch.Tensor, occu_mask: torch.Tensor):
         loss = []
+        if occu_mask.mean() == 0:
+            occu_mask = torch.ones_like(occu_mask)
 
         if hasattr(self, 'l1_weight'):
-            loss += [self.l1_weight * (im_orig - im_recons).abs()]
+            loss += [self.l1_weight * (im_orig - im_recons).abs() * occu_mask]
 
         if hasattr(self, 'ssim_weight'):
-            loss += [self.ssim_weight * self.SSIM(im_recons, im_orig)]
+            loss += [self.ssim_weight * self.SSIM(im_recons * occu_mask, im_orig * occu_mask)]
 
         if hasattr(self, 'ternary_weight'):
             loss += [self.ternary_weight *\
                      TernaryLoss(im_recons * occu_mask, im_orig * occu_mask)]
 
-        return sum([l.mean() for l in loss]) #/ occu_mask.mean()
+        return sum([l.mean() for l in loss]) / occu_mask.mean()
 
     def loss_smooth(self, flow, im_scaled):
         if self.smooth_args['degree'] == 2:
@@ -272,16 +275,19 @@ class unFlowLoss(nn.modules.Module):
             im1_recons = flow_warp(im2_scaled, flow[:, :2], pad='border')
             im2_recons = flow_warp(im1_scaled, flow[:, 2:], pad='border')
 
-            if i == 0:
-                if self.back_occ_only:
-                    occu_mask1 = 1 - get_occu_mask_backward(flow[:, 2:], theta=0.2)
-                    occu_mask2 = 1 - get_occu_mask_backward(flow[:, :2], theta=0.2)
-                else:
-                    occu_mask1 = 1 - get_occu_mask_bidirection(flow[:, :2], flow[:, 2:])
-                    occu_mask2 = 1 - get_occu_mask_bidirection(flow[:, 2:], flow[:, :2])
-            else:
-                occu_mask1 = F.interpolate(occu_mask1, (h, w), mode='nearest')
-                occu_mask2 = F.interpolate(occu_mask2, (h, w), mode='nearest')
+            #   Occlusion mask is broken, always returns zeros...
+            # if i == 0:
+            #     if self.back_occ_only:
+            #         occu_mask1 = 1 - get_occu_mask_backward(flow[:, 2:], theta=0.2)
+            #         occu_mask2 = 1 - get_occu_mask_backward(flow[:, :2], theta=0.2)
+            #     else:
+            #         occu_mask1 = 1 - get_occu_mask_bidirection(flow[:, :2], flow[:, 2:])
+            #         occu_mask2 = 1 - get_occu_mask_bidirection(flow[:, 2:], flow[:, :2])
+            # else:
+            #     occu_mask1 = F.interpolate(occu_mask1, (h, w), mode='nearest')
+            #     occu_mask2 = F.interpolate(occu_mask2, (h, w), mode='nearest')
+
+            occu_mask1 = occu_mask2 = torch.ones_like(im1_scaled)
 
             loss_warp = self.loss_photometric(im1_scaled, im1_recons, occu_mask1)
 