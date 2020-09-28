34635cbe1b17f153329c914392c0ac13436d998d
Bryce Ferenczi <bryce.ferenczi@monashmotorsport.com>
2020-09-28 15:47:37 +1000

    Plot validation comparison between multiple models.

--- nnet_training/experiment_comp.py
+++ nnet_training/experiment_comp.py
@@ -6,31 +6,61 @@ __email__ = "bryce.ferenczi@monashmotorsport.com"
 import os
 import argparse
 from pathlib import Path
+from typing import List, Dict
 
+import matplotlib.pyplot as plt
 from nnet_training.utilities.metrics import MetricBaseClass
+
+__all__ = ['compare_experiments']
+
+def compare_experiments(experiment_list: List[Dict[str, Path]]):
+    """
+    Given a list of experiments (Dictionary containing path), compares each of them to one another
+    """
+    for experiment in experiment_list:
+        for filename in os.listdir(experiment["path"]):
+            if filename.endswith(".hdf5"):
+                m_type = filename.replace("_data.hdf5", "")
+                experiment[m_type] = MetricBaseClass(
+                    savefile=filename, base_dir=experiment["path"], main_metric="")
+
+    for metric in exper_1:
+        if metric not in ["path", "name"] and metric in exper_2.keys():
+
+            data_1 = exper_1[metric].get_summary_data()
+            data_2 = exper_2[metric].get_summary_data()
+
+            fig, ax = plt.subplots(1, len(data_1), figsize=(18, 5))
+            fig.suptitle(f"Comparision between {metric} Validation Results")
+
+            for idx, data in enumerate(data_1):
+                ax[idx].plot(data_1[data]["Validation"], label=exper_1["name"])
+                ax[idx].plot(data_2[data]["Validation"], label=exper_2["name"])
+                ax[idx].set_title(f'{data} over Epochs')
+                ax[idx].set_xlabel('Epoch #')
+
+            fig.axes[-1].set_label((exper_1["name"], exper_2["name"]))
+            handles, labels = fig.axes[-1].get_legend_handles_labels()
+            fig.legend(handles, labels, loc='upper right')
+            plt.show(block=False)
 
 if __name__ == "__main__":
     ROOT_DIR = Path.cwd() / "torch_models"
     parser = argparse.ArgumentParser()
-    parser.add_argument('-e1', '--experiment1', default='1deaad09314bced71e32dd9c0a2fb481')
-    parser.add_argument('-e2', '--experiment2', default='03c05be48eb98c6b202ed0f26c6fe1b4')
+    parser.add_argument('-e1', '--experiment1', default='a8769a65bb701f77bce4d80053418b20')
+    parser.add_argument('-e2', '--experiment2', default='cdf66a3f794e9bd78247e48b4595ce82')
     args = parser.parse_args()
 
-    exper_1 = {"path": ROOT_DIR / args.experiment1}
-    exper_2 = {"path": ROOT_DIR / args.experiment2}
+    exper_1 = {
+        "name" : args.experiment1,
+        "path" : ROOT_DIR / args.experiment1
+    }
+    exper_2 = {
+        "name" : args.experiment2,
+        "path" : ROOT_DIR / args.experiment2
+    }
 
     expers = [exper_1, exper_2]
-    for exper in expers:
-        for filename in os.listdir(exper["path"]):
-            if filename.endswith(".hdf5"):
-                m_type = filename.strip("_data.hdf5")
-                exper[m_type] = MetricBaseClass(
-                    savefile=filename, base_dir=exper["path"], main_metric="")
 
-
-    for metric in exper_1:
-        if metric != "path" and metric in exper_2.keys():
-            exper_1[metric].plot_summary_data()
-            exper_2[metric].plot_summary_data()
-
+    compare_experiments(expers)
     input()
