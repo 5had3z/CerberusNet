a13a1dec8772a268a21c11af45302dae8a9a7cb4
Bryce Ferenczi <bryce.ferenczi@monashmotorsport.com>
2020-09-28 17:04:09 +1000

    Refactor/Making pylint happy

--- nnet_training/experiment_comp.py
+++ nnet_training/experiment_comp.py
@@ -34,47 +34,46 @@ def compare_experiments(experiment_list: List[Dict[str, Path]]):
     for objective in objectives:
         # Check to see if the objective is done and tracked
         # in an experiment and add it to the dictionary to be plotted
-        data_dict = {}
+        experiment_data = {}
         for experiment in experiment_list:
             if objective in experiment.keys():
-                data_dict[experiment["name"]] = experiment[objective].get_summary_data()
+                experiment_data[experiment["name"]] = experiment[objective].get_summary_data()
 
-        sample_data = next(iter(data_dict.values()))
+        sample_data = next(iter(experiment_data.values()))
 
-        fig, ax = plt.subplots(1, len(sample_data), figsize=(18, 5))
+        fig, axis = plt.subplots(1, len(sample_data), figsize=(18, 5))
         fig.suptitle(f"Comparision between {objective} Validation Results")
 
         # Plot each metric on a different subplot
         for idx, metric in enumerate(sample_data):
-            for name, summary_dict in data_dict.items():
-                ax[idx].plot(summary_dict[metric]["Validation"], label=name)
+            for name, summary_dict in experiment_data.items():
+                axis[idx].plot(summary_dict[metric]["Validation"], label=name)
 
-            ax[idx].set_title(f'{metric} over Epochs')
-            ax[idx].set_xlabel('Epoch #')
+            axis[idx].set_title(f'{metric} over Epochs')
+            axis[idx].set_xlabel('Epoch #')
 
-        handles, labels = fig.axes[-1].get_legend_handles_labels()
-        fig.legend(handles, labels, loc='lower center')
+        fig.legend(*fig.axes[-1].get_legend_handles_labels(), loc='lower center')
         plt.show(block=False)
 
     plt.show()
 
 if __name__ == "__main__":
     ROOT_DIR = Path.cwd() / "torch_models"
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-e', '--experiments', nargs='+',
-        default=['a8769a65bb701f77bce4d80053418b20', 'cdf66a3f794e9bd78247e48b4595ce82'])
-    args = parser.parse_args()
+    PARSER = argparse.ArgumentParser()
+    PARSER.add_argument('-e', '--experiments', nargs='+',
+                        default=['a8769a65bb701f77bce4d80053418b20',
+                                 'cdf66a3f794e9bd78247e48b4595ce82'])
 
-    expers = []
-    for exper in args.experiments:
-        expers.append({
+    EXPER_LIST = []
+    for exper in PARSER.parse_args().experiments:
+        EXPER_LIST.append({
             "name" : exper,
             "path" : ROOT_DIR / exper
         })
 
-    expers.append({
+    EXPER_LIST.append({
         "name" : '424199045aa062d24ef1fbd49ec1b6bc',
         "path" : ROOT_DIR / '424199045aa062d24ef1fbd49ec1b6bc'
     })
 
-    compare_experiments(expers)
+    compare_experiments(EXPER_LIST)
