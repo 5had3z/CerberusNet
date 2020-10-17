dbc1124aef851d47968452c6f314f35f5e194628
Bryce Ferenczi <bryce.ferenczi@monashmotorsport.com>
2020-10-17 22:21:59 +1100

    Moved the loop implementation into the grid sampler.

--- runtime/cerberus_net/trt_plugins/grid_sampler.cu
+++ runtime/cerberus_net/trt_plugins/grid_sampler.cu
@@ -2,8 +2,36 @@
 #include "trt_utils.hpp"
 #include "cuda_fp16.h"
 
-static inline bool within_bounds_2d(int64_t h, int64_t w, int64_t H, int64_t W) {
-    return h >= 0 && h < H && w >= 0 && w < W;
+#include <limits>
+
+// CUDA: grid stride looping
+//
+// int64_t _i_n_d_e_x specifically prevents overflow in the loop increment.
+// If input.numel() < INT_MAX, _i_n_d_e_x < INT_MAX, except after the final
+// iteration of the loop where _i_n_d_e_x += blockDim.x * gridDim.x can be
+// greater than INT_MAX.  But in that case _i_n_d_e_x >= n, so there are no
+// further iterations and the overflowed value in i=_i_n_d_e_x is not used.
+#define CUDA_KERNEL_LOOP_TYPE(i, n, index_type)                         \
+  int64_t _i_n_d_e_x = blockIdx.x * blockDim.x + threadIdx.x;           \
+  for (index_type i=_i_n_d_e_x; _i_n_d_e_x < (n); _i_n_d_e_x+=blockDim.x * gridDim.x, i=_i_n_d_e_x)
+
+#define CUDA_KERNEL_LOOP(i, n) CUDA_KERNEL_LOOP_TYPE(i, n, int)
+
+// CUDA: number of blocks for threads.
+inline int GET_BLOCKS(const int64_t N) {
+  assert(N > 0 && "CUDA kernel launch blocks must be positive");
+  constexpr int64_t max_int = std::numeric_limits<int>::max();
+
+  // Round up division for positive number that cannot cause integer overflow
+  auto block_num = (N - 1) / CUDA_NUM_THREADS + 1;
+  assert(block_num <= max_int && "Can't schedule too many blocks on CUDA device");
+
+  return static_cast<int>(block_num);
+}
+
+static __forceinline__ __device__
+bool within_bounds_2d(int h, int w, int H, int W) {
+  return h >= 0 && h < H && w >= 0 && w < W;
 }
 
 // Unnormalizes a coordinate from the -1 to +1 scale to its pixel index value,
